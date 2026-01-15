3412698e21f2af38cc926903607e393b01ebaad8
Justiniscoding <justinlasinski@gmail.com>
2026-01-15 18:41:28 -0500

    first iteration of turret system

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,9 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.TurretMoveCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.TurretSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -21,6 +23,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+	private final TurretSubsystem turretSubsystem = new TurretSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -49,6 +52,9 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+
+		m_driverController.a().whileTrue(new TurretMoveCommand(turretSubsystem, TurretSubsystem.TurretConstants.kTurretForwardPosition));
+		m_driverController.y().whileTrue(new TurretMoveCommand(turretSubsystem, TurretSubsystem.TurretConstants.kTurretBackwardPosition));
   }
 
   /**
---
+++ src/main/java/frc/robot/commands/TurretMoveCommand.java
@@ -0,0 +1,39 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.subsystems.TurretSubsystem;
+
+public class TurretMoveCommand extends Command {
+	private TurretSubsystem turretSubsystem;
+	private double position;
+
+	public TurretMoveCommand(TurretSubsystem turretSubsystem, double position) {
+		this.turretSubsystem = turretSubsystem;
+		addRequirements(turretSubsystem);
+		this.position = position;
+	}
+
+	@Override
+	public void initialize() {
+		turretSubsystem.moveToPosition(position);
+	}
+
+	@Override
+	public void execute() {
+	}
+
+	@Override
+	public void end(boolean interrupted) {
+	}
+
+	@Override
+	public boolean isFinished() {
+		if (Math.abs(turretSubsystem.getTurretPosition()
+				- position) > TurretSubsystem.TurretConstants.kTurretAccuracyThreshold) {
+			return true;
+		}
+
+		return false;
+	}
+}

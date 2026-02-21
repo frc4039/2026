9e68555d4728f37edaf401772c268fa270b3f669
MTBWill13 <willthomas561@gmail.com>
2026-02-21 09:34:59 -0500

    Removed extra import

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import java.util.Optional;
 
-import edu.wpi.first.hal.PowerDistributionFaults;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
